2e8705c5e620095353b058212eac2c5c7b196b48
Ryan Clark <ryan.clark.j@gmail.com>
2015-04-13 16:06:13 -0700

    only crop wgs84 features. add tests that compare fixtures to ogr2ogr outputs

--- index.js
+++ index.js
@@ -12,10 +12,9 @@ module.exports = function(infile, outfile) {
   var inLayer = inDs.layers.get(0);
   var outLayer = outDs.layers.create(inLayer.name, sm, inLayer.geomType);
   var toSm = new gdal.CoordinateTransformation(inLayer.srs, sm);
-  var toNative = new gdal.CoordinateTransformation(wgs84, inLayer.srs);
 
-  try { world.transform(toNative); }
-  catch (err) { } // noop, will leave world === null and we won't crop anything
+  // only crop WGS84 features
+  if (!inLayer.srs.isSame(wgs84)) world = null;
 
   inLayer.fields.forEach(function(field) {
     outLayer.fields.add(field);
@@ -26,13 +25,13 @@ module.exports = function(infile, outfile) {
     var geom = projected.getGeometry();
 
     // Originally null geometries are ok to pass through
-    if (!geom) return outLayer.features.add(projected);
+    if (!geom || geom.isEmpty()) return outLayer.features.add(projected);
 
     // If we can crop features, do it
     if (world) geom = geom.intersection(world);
 
     // If geom is null at this point, that means it got cropped out
-    if (!geom) return;
+    if (!geom || geom.isEmpty()) return;
 
     // Otherwise, transform the feature, pass it through to the output
     geom.transform(toSm);
--- test/index.test.js
+++ test/index.test.js
@@ -10,9 +10,35 @@ var nullgeom = path.join(__dirname, 'fixtures', 'nullgeom', 'null_geom.shp');
 var poles = path.join(__dirname, 'fixtures', 'poles', 'poles.shp');
 var stateplane = path.join(__dirname, 'fixtures', 'stateplane', 'stateplane.shp');
 var states = path.join(__dirname, 'fixtures', 'states', 'states.shp');
+var expectedwgs84 = path.join(__dirname, 'expected', 'wgs84', 'wgs84.shp');
+var expectednullgeom = path.join(__dirname, 'expected', 'nullgeom', 'null_geom.shp');
+var expectedpoles = path.join(__dirname, 'expected', 'poles', 'poles.shp');
+var expectedstateplane = path.join(__dirname, 'expected', 'stateplane', 'stateplane.shp');
+var expectedstates = path.join(__dirname, 'expected', 'states', 'states.shp');
 
 function truncate(num) {
   return Math.floor(num * Math.pow(10, 6)) / Math.pow(10, 6);
+}
+
+function truncatedExtent(extent) {
+  return [
+    truncate(extent.minX),
+    truncate(extent.minY),
+    truncate(extent.maxX),
+    truncate(extent.maxY)
+  ];
+}
+
+function compare(actual, expected, assert) {
+  actual = gdal.open(actual);
+  expected = gdal.open(expected);
+
+  assert.equal(actual.layers.count(), expected.layers.count(), 'matches ogr2ogr layer count');
+
+  actual = actual.layers.get(0);
+  expected = expected.layers.get(0);
+  assert.deepEqual(truncatedExtent(actual.getExtent()), truncatedExtent(expected.getExtent()), 'matches ogr2ogr extent');
+  assert.equal(actual.features.count(), expected.features.count(), 'matches ogr2ogr feature count');
 }
 
 test('reprojects', function(assert) {
@@ -73,22 +99,7 @@ test('reprojects', function(assert) {
     assert.deepEqual(geojson, expectedGeom, 'checked feature has proper coordinates');
   });
 
-  assert.end();
-});
-
-test('Allow null geometry', function(assert) {
-  var outfile = path.join(os.tmpdir(), crypto.randomBytes(8).toString('hex') + '.shp');
-  wmshp(nullgeom, outfile);
-
-  var ds = gdal.open(outfile);
-
-  ds.layers.forEach(function(layer) {
-    assert.equal(layer.features.count(), 1, 'reprojected all features');
-    var feature = layer.features.get(0);
-    var geojson = feature.getGeometry();
-
-    assert.deepEqual(geojson, null, 'checked feature has proper coordinates');
-  });
+  compare(outfile, expectedwgs84, assert);
 
   assert.end();
 });
@@ -135,6 +146,25 @@ test('reproject from a folder', function(assert) {
   });
 });
 
+test('Allow null geometry', function(assert) {
+  var outfile = path.join(os.tmpdir(), crypto.randomBytes(8).toString('hex') + '.shp');
+  wmshp(nullgeom, outfile);
+
+  var ds = gdal.open(outfile);
+
+  ds.layers.forEach(function(layer) {
+    assert.equal(layer.features.count(), 1, 'reprojected all features');
+    var feature = layer.features.get(0);
+    var geojson = feature.getGeometry();
+
+    assert.deepEqual(geojson, null, 'checked feature has proper coordinates');
+  });
+
+  compare(outfile, expectednullgeom, assert);
+
+  assert.end();
+});
+
 test('reproject features that go to 90/-90', function(assert) {
   var outfolder = path.join(os.tmpdir(), crypto.randomBytes(8).toString('hex'));
   wmshp(poles, outfolder);
@@ -150,6 +180,8 @@ test('reproject features that go to 90/-90', function(assert) {
     ['.dbf', '.prj', '.shp', '.shx'].forEach(function(extension) {
       assert.ok(extensions.indexOf(extension) > -1, extension + ' file created');
     });
+
+    compare(outfolder, expectedpoles, assert);
 
     assert.end();
   });
@@ -171,6 +203,8 @@ test('reprojects shapefiles in stateplane coordinate system', function(assert) {
       assert.ok(extensions.indexOf(extension) > -1, extension + ' file created');
     });
 
+    compare(outfolder, expectedstateplane, assert);
+
     assert.end();
   });
 });
@@ -190,6 +224,8 @@ test('reprojects shapefiles in lambert coordinate system', function(assert) {
     ['.dbf', '.prj', '.shp', '.shx'].forEach(function(extension) {
       assert.ok(extensions.indexOf(extension) > -1, extension + ' file created');
     });
+
+    compare(outfolder, expectedstates, assert);
 
     assert.end();
   });
